539fb812ef627a1443c54081d69fe68f32897e5a
EmineBC <eminebahce@gmail.com>
2019-03-27 17:24:47 +0100

    url fetch action created

---
+++ src/actions/quotepage.js
@@ -0,0 +1,21 @@
+import request from 'superagent';
+
+const baseURL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+const Quotes_FETCHED = 'Quotes_FETCHED';
+
+export const loadQuotes = () =>{
+    return(dispatch) => {
+        request(baseURL).then(response => {
+            console.log(response.body);
+            dispatch(quotesFetched(response.body));
+        }).catch(error => console.log(error));
+    }
+}
+
+const quotesFetched = (quotes) => ({
+   type: Quotes_FETCHED,
+   payload:{
+       quotes
+   }
+});
+
---
+++ src/components/QuotePage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {connect} from "react-redux";
+
+export default class QuotePage extends React{
+
+    componentDidMount(){
+        //actiondan fetch yapacak özlü sözleri
+    }
+
+    render(){
+        return;
+    }
+}
